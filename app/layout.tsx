f93275b405bf42bfaf4670ae2e8d0e966c5130da
Akshaygore1 <akshaygore2301@gmail.com>
2025-03-15 23:51:28 +0530

    Revert "migrate to next 15"
    This reverts commit 1e81524c20e9caaeba8c6559058d0dcc65c46d6e.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,22 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-import "./globals.css";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Fake Stake",
-  description: "This is Fake Stake",
+	title: 'Fake Stake',
+	description: 'This is Fake Stake',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} $ antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
