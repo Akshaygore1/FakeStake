a015755fe682371451387a64faa7682a435f8f12
Akshaygore1 <akshaygore2301@gmail.com>
2025-03-18 02:18:48 +0530

    added slider for dice

--- app/_components/Dice/DiceComponent.tsx
+++ app/_components/Dice/DiceComponent.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@/components/ui/button";
-import CustomSlider from "./DiceSlider";
+import { Slider } from "@/components/ui/slider";
 
 export default function DiceComponent() {
   return (
     <div className="w-full aspect-square bg-gray-800/30 backdrop-blur-sm border border-gray-700 rounded-xl p-8 flex items-center justify-center">
-      <div className="text-4xl font-bold text-gray-400">
-        <div className="w-96">
-          {/* <CustomSlider />
-           */}
+      <div className="p-4 bg-gray-600 rounded-3xl relative">
+        <div className="text-4xl p-2 font-bold text-gray-400 bg-gray-900 rounded-lg">
+          <div className="w-96">
+            <Slider />
+          </div>
         </div>
       </div>
     </div>
