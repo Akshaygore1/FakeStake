b79034f40ed79258febacc45fb1a5a4af3733847
Akshaygore1 <akshaygore2301@gmail.com>
2025-08-29 14:11:25 +0530

    Added posthog

---
+++ instrumentation-client.ts
@@ -0,0 +1,10 @@
+import posthog from "posthog-js"
+
+// Initialize PostHog client
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: "/ingest",
+  ui_host: "https://us.posthog.com",
+  defaults: '2025-05-24',
+  capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+  debug: process.env.NODE_ENV === "development",
+});
